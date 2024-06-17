d51fc62f5bc11a42ddc3b3a6086a89695e860252
heather-jp <163785729+heather-jp@users.noreply.github.com>
2024-06-17 18:29:43 +0100

    Added basic form handling and Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+function generateActivityIdeas(event) {
+  event.preventDefault();
 
+  new Typewriter("#activity-ideas", {
+    strings:
+      "If you are spending only 1 Day: Marvel at the Eiffel Tower and soak up the panoramic city views. Take a stroll along the Seine River and admire the iconic landmarks lining its banks. Visit the Louvre Museum and see masterpieces like the Mona Lisa.",
+    autoStart: true,
+    delay: 1,
+  });
+}
+
+let activityFormelement = document.querySelector(
+  "#activity-idea-generator-form"
+);
+activityFormelement.addEventListener("submit", generateActivityIdeas);
